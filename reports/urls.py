f27240bd2d0e7b926fc03fb1c5155bd32278c29f
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-02-20 11:23:35 +0000

    Change Navigation Bar

--- reports/urls.py
+++ reports/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import show_todays_date, class_list_view, update_lessons, update_days, class_print
 
 urlpatterns = [
-    path('today/', show_todays_date, name='show_todays_date'),
+    path('today/', show_todays_date, name='show_todays_data'),
     path('class/', class_list_view, name='class_list_view'),
     path('update-days', update_days, name='update-days'),
     path('update-lessons', update_lessons, name='update-lessons'),

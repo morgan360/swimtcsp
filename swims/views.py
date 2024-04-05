c02f7c88e26827115b6caab243dc1ae8e49b9dee
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-05 15:58:42 +0100

    "Axes Lock template"

--- swims/views.py
+++ swims/views.py
@@ -70,7 +70,7 @@ def product_detail(request, id, slug):
                 OrderItem.objects.create(order=order, variant=variant, quantity=quantity)
 
             # Generate a unique order reference using the order ID
-            order_ref = f"{order.id}}"
+            order_ref = order.id
 
             # Redirect to payment initiation with total amount and order reference
             return redirect(reverse('boipa:initiate_payment_session', kwargs={'order_ref': order_ref, 'total_price': str(total_amount)}))

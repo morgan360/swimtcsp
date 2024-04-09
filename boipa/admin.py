d0d13828ba296db811abffef0030eea572b98afa
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-09 11:43:37 +0100

    "Swim and LessonOrderNotifications"

--- boipa/admin.py
+++ boipa/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SwimOrderPaymentNotification, LessonOrderPaymentNotification
+from .models import SwimOrderPaymentNotification, LessonOrderPaymentNotification, SchoolOrderPaymentNotification
 
 
 class SwimOrderPaymentNotificationAdmin(admin.ModelAdmin):
@@ -15,5 +15,15 @@ class LessonOrderPaymentNotificationAdmin(admin.ModelAdmin):
         'txId',  'merchantTxId', 'amount', 'status')
     search_fields = ('txId', 'merchantTxId', 'amount', 'status')
 
-
 admin.site.register(LessonOrderPaymentNotification, LessonOrderPaymentNotificationAdmin)
+
+
+class SchoolOrderPaymentNotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        'txId',  'merchantTxId', 'amount', 'status')
+    search_fields = ('txId', 'merchantTxId', 'amount', 'status')
+
+admin.site.register(SchoolOrderPaymentNotification, SchoolOrderPaymentNotificationAdmin)
+
+
+
--- schools_orders/admin.py
+++ schools_orders/admin.py
@@ -9,21 +9,9 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product', 'term']
 
-
-def order_payment(obj):
-    url = obj.get_stripe_url()
-    if obj.stripe_id:
-        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
-        return mark_safe(html)
-    return ''
-
-
-order_payment.short_description = 'Stripe payment'
-
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'paid', 'user', order_payment,
+    list_display = ['id', 'paid', 'user',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]

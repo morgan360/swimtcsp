552335dbec52489de373555a3a8b3f5b426e43c0
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-16 19:38:00 +0100

    "Deleted handling"

--- boipa/views.py
+++ boipa/views.py
@@ -20,6 +20,7 @@ from schools_orders.models import Order as SchoolOrder
 from django.http import QueryDict
 from django.urls import reverse
 from lessons_bookings.utils.enrollment import handle_lessons_enrollment
+
 # Load environment variables
 load_dotenv()
 
@@ -34,7 +35,6 @@ PAYMENT_FORM_URL = os.getenv('HPP_FORM')  # URL for the payment form
 NGROK = os.getenv('NGROK')  # For ip tunnel from BOIPA
 HPP_FORM = os.getenv('HPP_FORM')
 timestamp = time.strftime("%Y%m%d%H%M%S")
-
 
 
 def initiate_boipa_payment_session(request, order_ref, total_price):
@@ -60,9 +60,10 @@ def initiate_boipa_payment_session(request, order_ref, total_price):
         "merchantNotificationUrl": NGROK + reverse('boipa:payment_notification'),
         "merchantLandingPageRedirectMethod": "GET",
     }
-    print('PAYLOAD',payload)
+    print('PAYLOAD', payload)
     # Send the request to BOIPA to initiate the payment session
-    response = requests.post(BOIPA_TOKEN_URL, data=payload, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    response = requests.post(BOIPA_TOKEN_URL, data=payload,
+                             headers={'Content-Type': 'application/x-www-form-urlencoded'})
     if response.status_code == 200:
         # Extract the session token from the response
         session_token = response.json().get('token')
@@ -75,10 +76,6 @@ def initiate_boipa_payment_session(request, order_ref, total_price):
         return render(request, 'error.html', {'error_message': 'Failed to initiate payment session.'})
 
 
-def error_view(request):
-    # Example error handling logic
-    error_message = "An unexpected error has occurred."
-    return render(request, 'error.html', {'error_message': error_message})
 
 
 # Also in views.py
@@ -88,9 +85,22 @@ def payment_response(request):
     Handles the payment response from BOIPA.
     """
     result = request.GET.get('result')
-    merchant_tx_id = request.GET.get('merchantTxId')
+    merchantTxId = request.GET.get('merchantTxId')
+    source_prefix, order_id_str = merchantTxId.split("_", 1)
+    order_ref = merchantTxId
+    order_id = int(order_id_str)
 
     if result == "success":
+        if source_prefix == "swim":
+            order = SwimOrder.objects.get(id=order_id)
+        elif source_prefix == "lessons":
+            order = LessonOrder.objects.get(id=order_id)
+        elif source_prefix == "schools":
+            order = SchoolOrder.objects.get(id=order_id)
+
+            order.paid = True
+            order.save()
+
         # Payment was successful
         return render(request, 'payment_success.html', {'merchant_tx_id': merchant_tx_id})
     elif result == "failure":
@@ -100,131 +110,95 @@ def payment_response(request):
         # Unrecognized result
         return render(request, 'error.html', {'error_message': 'Unknown payment response.'})
 
-
+###### Notifications #######
 @csrf_exempt  # Disable CSRF protection for this endpoint
 def payment_notification(request):
     if request.method == 'POST':
         data = QueryDict(request.body)
-# Extracting the necessary information
-        txId = data.get('txId')
+        # Extracting the necessary information
         merchantTxId = data.get('merchantTxId')
-        status = data.get('status')
-        try:
-            source_prefix, order_id_str = merchantTxId.split("_", 1)
-            order_id = int(order_id_str)
-        except ValueError:
-            # If conversion fails, respond with an error indicating bad data
-            return HttpResponse("Invalid order ID format", status=400)
-
+        source_prefix, order_id_str = merchantTxId.split("_", 1)
+        order_id = int(order_id_str)
         if source_prefix == 'swims':
-            try:
-                order = SwimOrder.objects.get(id=order_id)  # Assuming merchantTxId is the Order ID
-                order.txId = txId
-                order.payment_status = status
-                if status == 'SET_FOR_CAPTURE' or status == 'CAPTURED':
-                    order.paid = True
-                else:
-                    order.paid = False
-                order.save()
+            # Parse the URL-encoded form data
+            data = QueryDict(request.body)
+            order_obj = SwimOrder.objects.get(id=order_id)
 
-                # Create a payment notification record
-                SwimOrderPaymentNotification.objects.create(
-                    order=order,
-                    txId=txId,
-                    merchantTxId=merchantTxId,
-                    country=data.get('country'),
-                    amount=data.get('amount'),
-                    currency=data.get('currency'),
-                    action=data.get('action'),
-                    # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
-                    # auth_code=data.get('auth_code'),
-                    acquirer=data.get('acquirer'),
-                    acquirerAmount=data.get('acquirerAmount'),
-                    merchantId=data.get('merchantId'),
-                    brandId=data.get('brandId'),
-                    customerId=data.get('customerId'),
-                    acquirerCurrency=data.get('acquirerCurrency'),
-                    paymentSolutionId=data.get('paymentSolutionId'),
-                    status=data.get('status'), )
+            # Create a payment notification record
+            SwimOrderPaymentNotification.objects.create(
+                order=order_obj,
+                txId=data.get('txId'),
+                merchantTxId=data.get('merchantTxId'),
+                country=data.get('country'),
+                amount=data.get('amount'),
+                currency=data.get('currency'),
+                action=data.get('action'),
+                # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
+                # auth_code=data.get('auth_code'),
+                acquirer=data.get('acquirer'),
+                acquirerAmount=data.get('acquirerAmount'),
+                merchantId=data.get('merchantId'),
+                brandId=data.get('brandId'),
+                customerId=data.get('customerId'),
+                acquirerCurrency=data.get('acquirerCurrency'),
+                paymentSolutionId=data.get('paymentSolutionId'),
+                status=data.get('status'),
+            )
 
-                return HttpResponse("Notification processed successfully", status=200)
-            except Order.DoesNotExist:
-                # Handle case where the order does not exist
-                return HttpResponse("Order not found", status=400)
+            # Return a successful HTTP response
+            return HttpResponse('Payment processed successfully', status=200)
 
         elif source_prefix == 'lessons':
-            try:
-                order = LessonOrder.objects.get(id=order_id)  # Assuming merchantTxId is the Order ID
-                order.txId = txId
-                order.payment_status = status
-                if status == 'SET_FOR_CAPTURE' or status == 'CAPTURED':
-                    order.paid = True
-                    # Enroll in the lesson
-                    handle_lessons_enrollment(order)
-                else:
-                    order.paid = False
-                order.save()
-
-                # Create a payment notification record
-                LessonOrderPaymentNotification.objects.create(
-                    order=order,
-                    txId=txId,
-                    merchantTxId=merchantTxId,
-                    country=data.get('country'),
-                    amount=data.get('amount'),
-                    currency=data.get('currency'),
-                    action=data.get('action'),
-                    # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
-                    # auth_code=data.get('auth_code'),
-                    acquirer=data.get('acquirer'),
-                    acquirerAmount=data.get('acquirerAmount'),
-                    merchantId=data.get('merchantId'),
-                    brandId=data.get('brandId'),
-                    customerId=data.get('customerId'),
-                    acquirerCurrency=data.get('acquirerCurrency'),
-                    paymentSolutionId=data.get('paymentSolutionId'),
-                    status=data.get('status'), )
-
-                return HttpResponse("Notification processed successfully", status=200)
-            except Order.DoesNotExist:
-                # Handle case where the order does not exist
-                return HttpResponse("Order not found", status=400)
+            # Parse the URL-encoded form data
+            data = QueryDict(request.body)
+            order_obj = LessonOrder.objects.get(id=order_id)
+            # Create a payment notification record
+            LessonOrderPaymentNotification.objects.create(
+                order=order_obj,
+                txId=data.get('txId'),
+                merchantTxId=data.get('merchantTxId'),
+                country=data.get('country'),
+                amount=data.get('amount'),
+                currency=data.get('currency'),
+                action=data.get('action'),
+                # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
+                # auth_code=data.get('auth_code'),
+                acquirer=data.get('acquirer'),
+                acquirerAmount=data.get('acquirerAmount'),
+                merchantId=data.get('merchantId'),
+                brandId=data.get('brandId'),
+                customerId=data.get('customerId'),
+                acquirerCurrency=data.get('acquirerCurrency'),
+                paymentSolutionId=data.get('paymentSolutionId'),
+                status=data.get('status'),
+                 )
+            return HttpResponse('Payment processed successfully', status=200)
 
         elif source_prefix == 'schools':
-            try:
-                order = SchoolOrder.objects.get(id=order_id)  # Assuming merchantTxId is the Order ID
-                order.txId = txId
-                order.payment_status = status
-                if status == 'SET_FOR_CAPTURE' or status == 'CAPTURED':
-                    order.paid = True
-                else:
-                    order.paid = False
-                order.save()
+            data = QueryDict(request.body)
+            order_obj =SchoolOrder.objects.get(id=order_id)
+            # Create a payment notification record
+            SchoolOrderPaymentNotification.objects.create(
+                order=order_obj,
+                txId=data.get('txId'),
+                merchantTxId=data.get('merchantTxId'),
+                country=data.get('country'),
+                amount=data.get('amount'),
+                currency=data.get('currency'),
+                action=data.get('action'),
+                # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
+                # auth_code=data.get('auth_code'),
+                acquirer=data.get('acquirer'),
+                acquirerAmount=data.get('acquirerAmount'),
+                merchantId=data.get('merchantId'),
+                brandId=data.get('brandId'),
+                customerId=data.get('customerId'),
+                acquirerCurrency=data.get('acquirerCurrency'),
+                paymentSolutionId=data.get('paymentSolutionId'),
+                status=data.get('status'), )
 
-                # Create a payment notification record
-                SchoolOrderPaymentNotification.objects.create(
-                    order=order,
-                    txId=txId,
-                    merchantTxId=merchantTxId,
-                    country=data.get('country'),
-                    amount=data.get('amount'),
-                    currency=data.get('currency'),
-                    action=data.get('action'),
-                    # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
-                    # auth_code=data.get('auth_code'),
-                    acquirer=data.get('acquirer'),
-                    acquirerAmount=data.get('acquirerAmount'),
-                    merchantId=data.get('merchantId'),
-                    brandId=data.get('brandId'),
-                    customerId=data.get('customerId'),
-                    acquirerCurrency=data.get('acquirerCurrency'),
-                    paymentSolutionId=data.get('paymentSolutionId'),
-                    status=data.get('status'), )
+            return HttpResponse("Notification processed successfully", status=200)
 
-                return HttpResponse("Notification processed successfully", status=200)
-            except Order.DoesNotExist:
-                # Handle case where the order does not exist
-                return HttpResponse("Order not found", status=400)
-        else:
-            # If not a POST request, indicate it's an invalid request method
-            return HttpResponse("Invalid request method", status=405)
+    else:
+        # If not a POST request, indicate it's an invalid request method
+        return HttpResponse("Invalid request method", status=405)

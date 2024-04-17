970c0982484ce6f8a9701a972be484462b8ff554
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-17 21:39:34 +0100

    "3"

--- boipa/views.py
+++ boipa/views.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.conf import settings
+import os
+import logging
 import time
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -20,12 +22,13 @@ from schools_orders.models import Order as SchoolOrder
 from django.http import QueryDict
 from django.urls import reverse
 from lessons_bookings.utils.enrollment import handle_lessons_enrollment
+from django.db import transaction
 
 # Load environment variables
 load_dotenv()
 
 # Initialize logging
-logger = logging.getLogger(__name__)
+payments_logger = logging.getLogger('payments')
 
 # Environment variables
 BOIPA_MERCHANT_ID = os.getenv('BOIPA_MERCHANT_ID')
@@ -82,97 +85,99 @@ def payment_response(request):
     """
     Handles the payment response from BOIPA.
     """
+    payments_logger.debug(f"Received payment response: {request.GET.dict()}")
     result = request.GET.get('result')
     merchantTxId = request.GET.get('merchantTxId')
-    source_prefix, order_id_str = merchantTxId.split("_", 1)
-
-    order_id = int(order_id_str)
-    order_ref = order_id
 
     if result == "success":
-        if source_prefix == "swim":
-            order = SwimOrder.objects.get(id=order_id)
-        elif source_prefix == "lessons":
-            order = LessonOrder.objects.get(id=order_id)
-        elif source_prefix == "schools":
-            order = SchoolOrder.objects.get(id=order_id)
-
-        order.paid = True
-        order.save()
-
         # Payment was successful
-        return render(request, 'payment_success.html', {'order_ref': order_ref, 'message':result})
+        return render(request, 'payment_success.html', {'order_ref': merchantTxId, 'message':result})
 
     elif result == "failure":
         # Payment failed
-        return render(request, 'payment_failure.html', {'order_ref ': order_ref, 'message':result})
+        return render(request, 'payment_failure.html', {'order_ref ': merchantTxId, 'message':result})
     else:
         # Unrecognized result
         return render(request, 'error.html', {'error_message': 'Unknown payment response.'})
 
 ###### Notifications #######
+
 @csrf_exempt  # Disable CSRF protection for this endpoint
 def payment_notification(request):
     if request.method == 'POST':
+        payments_logger.debug(f"Received payment notification: {request.POST.dict()}")
         data = QueryDict(request.body)
         # Extracting the necessary information
         merchantTxId = data.get('merchantTxId')
         source_prefix, order_id_str = merchantTxId.split("_", 1)
         order_id = int(order_id_str)
+
         if source_prefix == 'swims':
+            with transaction.atomic():
+                order = SwimOrder.objects.get(id=order_id)
+                order.paid = True
+                order.save()
             # Parse the URL-encoded form data
             data = QueryDict(request.body)
-            order_obj = SwimOrder.objects.get(id=order_id)
+            with transaction.atomic():
+                # Create a payment notification record
+                SwimOrderPaymentNotification.objects.create(
+                    order=order,
+                    txId=data.get('txId'),
+                    merchantTxId=data.get('merchantTxId'),
+                    country=data.get('country'),
+                    amount=data.get('amount'),
+                    currency=data.get('currency'),
+                    action=data.get('action'),
+                    # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
+                    # auth_code=data.get('auth_code'),
+                    acquirer=data.get('acquirer'),
+                    acquirerAmount=data.get('acquirerAmount'),
+                    merchantId=data.get('merchantId'),
+                    brandId=data.get('brandId'),
+                    customerId=data.get('customerId'),
+                    acquirerCurrency=data.get('acquirerCurrency'),
+                    paymentSolutionId=data.get('paymentSolutionId'),
+                    status=data.get('status'),
+                )
 
-            # Create a payment notification record
-            SwimOrderPaymentNotification.objects.create(
-                order=order_obj,
-                txId=data.get('txId'),
-                merchantTxId=data.get('merchantTxId'),
-                country=data.get('country'),
-                amount=data.get('amount'),
-                currency=data.get('currency'),
-                action=data.get('action'),
-                # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
-                # auth_code=data.get('auth_code'),
-                acquirer=data.get('acquirer'),
-                acquirerAmount=data.get('acquirerAmount'),
-                merchantId=data.get('merchantId'),
-                brandId=data.get('brandId'),
-                customerId=data.get('customerId'),
-                acquirerCurrency=data.get('acquirerCurrency'),
-                paymentSolutionId=data.get('paymentSolutionId'),
-                status=data.get('status'),
-            )
-
-            # Return a successful HTTP response
-            return HttpResponse('Payment processed successfully', status=200)
+                # Return a successful HTTP response
+                return HttpResponse('Payment processed successfully', status=200)
 
         elif source_prefix == 'lessons':
             # Parse the URL-encoded form data
             data = QueryDict(request.body)
-            order_obj = LessonOrder.objects.get(id=order_id)
+            merchantTxId = data.get('merchantTxId')
+            source_prefix, order_id_str = merchantTxId.split("_", 1)
+            order_id = int(order_id_str)
+
             # Create a payment notification record
-            LessonOrderPaymentNotification.objects.create(
-                order=order_obj,
-                txId=data.get('txId'),
-                merchantTxId=data.get('merchantTxId'),
-                country=data.get('country'),
-                amount=data.get('amount'),
-                currency=data.get('currency'),
-                action=data.get('action'),
-                # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
-                # auth_code=data.get('auth_code'),
-                acquirer=data.get('acquirer'),
-                acquirerAmount=data.get('acquirerAmount'),
-                merchantId=data.get('merchantId'),
-                brandId=data.get('brandId'),
-                customerId=data.get('customerId'),
-                acquirerCurrency=data.get('acquirerCurrency'),
-                paymentSolutionId=data.get('paymentSolutionId'),
-                status=data.get('status'),
-                 )
-            return HttpResponse('Payment processed successfully', status=200)
+            with transaction.atomic():
+                order = LessonOrder.objects.get(id=order_id)
+                order.paid = True
+                order.save()
+
+            with transaction.atomic():
+                LessonOrderPaymentNotification.objects.create(
+                    order=order,
+                    txId=data.get('txId'),
+                    merchantTxId=data.get('merchantTxId'),
+                    country=data.get('country'),
+                    amount=data.get('amount'),
+                    currency=data.get('currency'),
+                    action=data.get('action'),
+                    # Assuming auth_code and other details are extracted correctly from paymentSolutionDetails or similar
+                    # auth_code=data.get('auth_code'),
+                    acquirer=data.get('acquirer'),
+                    acquirerAmount=data.get('acquirerAmount'),
+                    merchantId=data.get('merchantId'),
+                    brandId=data.get('brandId'),
+                    customerId=data.get('customerId'),
+                    acquirerCurrency=data.get('acquirerCurrency'),
+                    paymentSolutionId=data.get('paymentSolutionId'),
+                    status=data.get('status'),
+                     )
+                return HttpResponse('Payment processed successfully', status=200)
 
         elif source_prefix == 'schools':
             data = QueryDict(request.body)
--- config/production_settings.py
+++ config/production_settings.py
@@ -1,5 +1,7 @@
 from config.base_settings import *
 from dotenv import load_dotenv
+import os
+import logging
 
 load_dotenv()  # loads the configs from .env
 
@@ -23,5 +25,45 @@ EMAIL_USE_TLS = True  # Use TLS (True for Gmail)
 EMAIL_HOST_USER = 'morganmcknight@gmail.com'  # Your Gmail email address
 EMAIL_HOST_PASSWORD = 'rkjxohiawwncphgp'  # Your Gmail password or an app password
 EMAIL_USE_SSL = False
+
+PAYMENTS_LOG_FILE_PATH =  '/home/morganmck/swimtcsp/logs/payments.log'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'payments_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': PAYMENTS_LOG_FILE_PATH,
+            'formatter': 'detailed',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'formatters': {
+        'detailed': {
+            'format': '{asctime} {levelname} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'loggers': {
+        'payments': {
+            'handlers': ['payments_file'],
+            'level': 'DEBUG',
+            'propagate': False,  # Prevent the payment logs from propagating to the root logger
+        },
+    },
+}
+
+
+
 
 FOOTER_MESSAGE = "Production Version"

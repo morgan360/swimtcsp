46f5df012bc27015299140c9202e7cf1bff52e2f
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-28 08:51:11 +0100

    "Added Schools to fray and tidy work"

--- config/production_settings.py
+++ config/production_settings.py
@@ -1,13 +1,14 @@
-from config.base_settings import *
-from dotenv import load_dotenv
 import os
 import logging
 import environ
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 # Initialize environ
 env = environ.Env()
 # Reading .env file
@@ -20,34 +21,27 @@ BOIPA_PASSWORD = env('BOIPA_PASSWORD')
 BOIPA_TOKEN_URL = env('BOIPA_TOKEN_URL')
 HPP_FORM = env('HPP_FORM')
 NGROK = env('NGROK', default='http://localhost:4040')
+SECRET_KEY = env('SECRET_KEY')
+DJANGO_SETTINGS_MODULE = env('DJANGO_SETTINGS_MODULE')
+BRAND_ID = env('BRAND_ID')
+BOIPA_PAYMENT_URL = env('BOIPA_PAYMENT_URL')
+
 CART_SESSION_ID = 'cart'
-load_dotenv()  # loads the configs from .env
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'morganmck$swimtcsp',
-        'USER': 'morganmck',
-        'PASSWORD': os.getenv("DB_PASSWORD"),  # Ensures the password is read from environment variables
-        'HOST': 'morganmck.mysql.eu.pythonanywhere-services.com',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },  # Make sure to close the OPTIONS dictionary correctly
-    }
+    'default': env.db('DATABASE_URL', default=f"mysql://morganmck:{env('DB_PASSWORD')}@morganmck.mysql.eu.pythonanywhere-services.com/morganmck$swimtcsp")
 }
-
 
 ALLOWED_HOSTS = ['tcsp-morganmck.eu.pythonanywhere.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True  # Use TLS (True for Gmail)
-EMAIL_HOST_USER = 'morganmcknight@gmail.com'  # Your Gmail email address
-EMAIL_HOST_PASSWORD = 'rkjxohiawwncphgp'  # Your Gmail password or an app password
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'morganmcknight@gmail.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # Use environment variable for security
 
-PAYMENTS_LOG_FILE_PATH =  '/home/morganmck/swimtcsp/logs/payments.log'
+PAYMENTS_LOG_FILE_PATH = '/home/morganmck/swimtcsp/logs/payments.log'
 
 LOGGING = {
     'version': 1,
@@ -79,12 +73,9 @@ LOGGING = {
         'payments': {
             'handlers': ['payments_file'],
             'level': 'DEBUG',
-            'propagate': False,  # Prevent the payment logs from propagating to the root logger
+            'propagate': False,
         },
     },
 }
-
-
-
 
 FOOTER_MESSAGE = "Production Version"

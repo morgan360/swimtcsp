f5843c8b3ab82f791a4b741d4253698e8fcb3153
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-05-01 13:11:42 +0100

    "Combined cart tidy work"

--- boipa/payment_functions.py
+++ boipa/payment_functions.py
@@ -33,6 +33,10 @@ def get_boipa_session_token(request, order_ref, total_price):
             "merchantChallengeInd": "01",
             "merchantDecReqInd": "N",
             "freeText": "Optional extra transaction info",
+            # New fields with dummy data
+            'customerAddressStreet': "123 Fake Street",
+            'customerAddressCity': "Dublin",
+            'customerAddressPostalCode': "D02 X285",
         }
 
         payments_logger.debug("Sending payload to API: %s", {k: v for k, v in payload.items() if k != 'password'})

70caa9ea9e30adc75a5c5f397eb5b16b5d1ca999
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2025-07-23 09:18:47 +0100

    "AAdd Tailwind CSS + DaisyUI setup with input.css for styling"

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/**/*.js',
+    './**/templates/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
+
+

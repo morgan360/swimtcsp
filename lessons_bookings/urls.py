0388ab88f713bd46bc5f15bdee36fb7c1d9ba1e3
Maeve Crossan <maevecrossan@gmail.com>
2025-08-27 10:29:51 +0100

    Wire up 'instructors' card and allow assignments via admin dashboard.

--- lessons_bookings/urls.py
+++ lessons_bookings/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import RegistrationWizardView
+from .views import RegistrationWizardView, instructor_assignments
 
 app_name = 'lessons_bookings'
 
 urlpatterns = [
     path('register/', RegistrationWizardView.as_view(), name='registration_wizard'),
+    path('instructors/', instructor_assignments, name='instructor_assignments'),
 ]
